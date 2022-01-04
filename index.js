299077b086a20a5aadc2851f062686401eca15c0
Phạm Minh Đức <duc0981173026@gmail.com>
2022-01-04 19:58:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const generate = async () => {
 
   if (!quote) return;
 
-  fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
+  fs.writeFileSync("README.md", `### :wave: Hi, I’m [@fishudroid](https://github.com/fishudroid). My real name is Phạm Minh Đức. \n\n  ### :postbox: Contact me: \n\n - Email: duc0981173026@gmail.com |\n duc0868243802@gmail.com | alonevnvnvn20@gmail.com | fishutechno.droidvn@gmail.com \n - Facebook: [@Ducky.LS.Vn](https://www.facebook.com/Ducky.LS.Vn) \n - Discord: @Nakiri Ayame#7916 \n - Telegram: @DuckyNA \n  - Zalo: +84889035565 <hr> \n\n ![Ayame](/ayame-nakiri-ayame.gif)_**${quote}**_\n\n${author}`);
 };
 
 generate();
